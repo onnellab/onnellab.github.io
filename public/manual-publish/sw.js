35f0d281fc634d44d020dd678b22722d29ef8268
onnellab <onnellab.app@gmail.com>
2026-07-24 14:21:23 +0900

    Add store review reply dashboard

--- public/manual-publish/sw.js
+++ public/manual-publish/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'onnellab-manual-publish-v11';
+const CACHE = 'onnellab-manual-publish-v12';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
